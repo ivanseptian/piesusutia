f5961ca9720cb0ad3bd0c2737ec9b0ff64d512ad
ivanseptian <37571629+ivanseptian@users.noreply.github.com>
2022-08-31 23:47:42 +0700

    fixing 2
    fixing 2

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -4,7 +4,7 @@ const ServiceCard = ({title, desc, image}) => {
   return (
     <div className="service-item">
         <div className="service-img mx-auto">
-            <img className="rounded-circle w-100 h-100 bg-light p-3" src={image} style={{objectFit: "cover"}} />
+            <img className="rounded-circle w-100 h-100 bg-light p-3" src={image} style={{objectFit: "cover"}} alt="pie susu tia" />
         </div>
         <div className="position-relative text-center bg-light rounded p-4 pb-5" style={{marginTop: "-75px"}}>
             <h5 className="font-weight-semi-bold mt-5 mb-3 pt-5">{title}</h5>
